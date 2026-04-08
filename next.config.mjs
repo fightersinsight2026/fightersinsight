3b55e197b2e429fd00785bdbcff6dd46ef0b11c4
fightersinsight2026 <fightersinsight26@gmail.com>
2026-04-08 13:20:35 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "res.cloudinary.com" },
       { protocol: "https", hostname: "cdn.fightersinsight.com" },
     ],
-  },
-  experimental: {
-    typedRoutes: false,
   },
 };
 
